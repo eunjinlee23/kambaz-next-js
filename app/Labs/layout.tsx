39ee9c308f4c6e5fcc027a00911101210cf1b3cc
eunjinlee23 <eunj.lee8585@gmail.com>
2025-10-06 21:55:37 -0400

    edit lab page

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,18 +1,18 @@
 import { ReactNode } from "react";
 import TOC from "./TOC";
+import { Container } from "react-bootstrap";
 
 export default function LabsLayout({
     children,
 }: Readonly<{ children: ReactNode }>) {
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <td valign="top" width="100px">
-                        <TOC />
-                    </td>
-                    <td valign="top">{children}</td>
-                </tr>
-            </tbody>
-        </table>
+        <Container>
+            <div>
+                <TOC />
+            </div>
+            <div>
+                {children}
+            </div>
+        </Container>
+
 );}
