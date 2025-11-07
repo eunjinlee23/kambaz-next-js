9b22d5bc109070cb9964dafa6f88ac88b49ea34b
eunjinlee23 <eunj.lee8585@gmail.com>
2025-11-07 03:08:36 -0500

    fix module reducer type

--- app/(Kambaz)/Courses/[cid]/Modules/reducer.ts
+++ app/(Kambaz)/Courses/[cid]/Modules/reducer.ts
@@ -6,18 +6,60 @@ const initialState = {
     modules: modules,
 };
 
+{
+    "_id": "M101",
+    "name": "Introduction to Rocket Propulsion",
+    "description": "Basic principles of rocket propulsion and rocket engines.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of Rocketry",
+        "description": "A brief history of rocketry and space exploration.",
+        "module": "M101"
+      },
+      {
+        "_id": "L102",
+        "name": "Rocket Propulsion Fundamentals",
+        "description": "Basic principles of rocket propulsion.",
+        "module": "M101"
+      },
+      {
+        "_id": "L103",
+        "name": "Rocket Engine Types",
+        "description": "Overview of different types of rocket engines.",
+        "module": "M101"
+      }
+    ]
+  },
+
+type Lesson = {
+    _id: string,
+    name: string,
+    description: string,
+    module: string
+};
+
+type Module = {
+    _id: string,
+    name: string,
+    description: string,
+    course: string,
+    lessons: Lesson[]
+}
+
 const modulesSlice = createSlice({
     name: "modules",
     initialState,
     reducers: {
         addModule: (state, { payload: module }) => {
-            const newModule = {
+            const newModule: Module = {
                 _id: uuidv4(),
                 lessons: [],
                 name: module.name,
                 course: module.course,
             }
-            state.modules = [...state.modules, newModule];
+            state.modules = [...state.modules, newModule] as Module[];
         },
 
         deleteModule: (state, { payload: moduleId }) => {
--- app/(Kambaz)/Enrollment/reducer.ts
+++ app/(Kambaz)/Enrollment/reducer.ts
@@ -10,12 +10,12 @@ const enrollmentsSlice = createSlice({
     initialState,
     reducers: {
         addEnrollment: (state, { payload: enrollment }) => {
-            const newEnrollment: unknown = {
+            const newEnrollment = {
                 _id: uuidv4(),
                 user: enrollment.user,
                 course: enrollment.course,
             }
-            state.enrollments = [...state.enrollments, newEnrollment] as unknown;
+            state.enrollments = [...state.enrollments, newEnrollment];
         },
 
         deleteEnrollment: (state, { payload: enrollmentId }) => {
@@ -25,6 +25,6 @@ const enrollmentsSlice = createSlice({
     },
 });
 
-export const { addEnrollment, deleteEnrollment, updateEnrollment, editEnrollment } =
+export const { addEnrollment, deleteEnrollment } =
     enrollmentsSlice.actions;
 export default enrollmentsSlice.reducer;
