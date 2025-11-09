581132f9ca96ab196752020c57d1cac3b877e5ce
eunjinlee23 <eunj.lee8585@gmail.com>
2025-11-09 13:10:35 -0500

    typo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,8 +20,8 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
     rules: {
-      "@typescript-eslint/no=explicit-any": "off",
-      
+      "@typescript-eslint/no-explicit-any": "off",
+
     },
   },
 ];
